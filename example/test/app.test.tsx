7e398be577f4dd682f63695b42c50ed5062bec95
hupe1980 <frankhuebner1980@gmail.com>
2021-01-22 19:31:35 +0100

    chore: Remove useless test

--- example/test/app.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "../src/app";
-
-test("renders badge", () => {
-  const { asFragment } = render(<App />);
-  expect(asFragment()).toMatchSnapshot();
-});
